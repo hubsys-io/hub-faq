009011202261c17850c2d2f6b1e0c439ef701150
Lucas Morales <lucasvmorales@protonmail.com>
2023-08-10 17:33:55 -0300

    feat: changing faq page

--- vue/src/components/FaqPage.vue
+++ vue/src/components/FaqPage.vue
@@ -1,29 +1,22 @@
 <template>
-  <div class="p-8">
+  <div class="px-8">
+  <div class="px-4">
     <div class="bg-white p-4 rounded-lg shadow-xl py-8 mt-12">
-      <h4 class="text-4xl font-bold text-gray-800 tracking-widest uppercase text-center">FAQ</h4>
-      <p class="text-center text-gray-600 text-sm mt-2">Here are some of the frequently asked questions</p>
-      <div class="space-y-12 px-2 xl:px-16 mt-12">
-        <div v-for="(faq, index) in paginatedFaqs" :key="index" class="mt-4 flex">
-          <div>
-            <div class="flex items-center h-16 border-l-4 border-japanese-laurel-800">
-              <span class="text-4xl text-japanese-laurel-800 px-4">Q.</span>
-            </div>
-            <div class="flex items-center h-16 border-l-4 border-gray-400">
-              <span class="text-4xl text-gray-400 px-4">A.</span>
-            </div>
-          </div>
-          <div>
-            <div class="flex items-center h-16">
-              <span class="text-lg text-japanese-laurel-800 font-bold">{{ faq.question }}</span>
-            </div>
-            <div class="flex items-center py-2">
-              <span class="text-gray-500">{{ faq.answer }}</span>
-            </div>
+        <h4 class="text-4xl font-bold text-gray-800 tracking-widest uppercase text-center">FAQ</h4>
+        <p class="text-center text-gray-600 text-sm mt-2">Here are some of the frequently asked questions</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-12 px-2 xl:px-12 mt-4">
+            <div v-for="(faq, index) in paginatedFaqs" :key="index">
+              <div class="flex space-x-8 mt-8">
+                  <div>
+                      <h4 class="text-xl font-bold text-gray-700">{{ faq.question }}</h4>
+                      <p class="text-gray-600 my-2">{{ faq.answer }}</p>
+                      <!-- <a href="#" class="text-japanese-laurel-800 hover:text-japanese-laurel-900 hover:underline capitalize" title="Read More">Read More</a> -->
+                  </div>
+              </div>
           </div>
         </div>
-      </div>
     </div>
+</div>
 
     <div class="flex justify-center mt-4 mb-4">
       <nav>
@@ -61,23 +54,35 @@ export default {
     return {
       faqs: [
         {
-          question: "Bombana é gay?",
-          answer: "Sim!"
+          question: "Lorem ipsum dolor sit amet?",
+          answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!!"
         },
         {
-          question: "Bombana é gay?",
-          answer: "Sim!"
+          question: "Consectetur adipisicing elit?",
+          answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!"
         },
         {
-          question: "Bombana é gay?",
-          answer: "Sim!"
+          question: "Neque eos, dignissimos provident reiciendis debitis?",
+          answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!"
+        },
+        {
+          question: "Repudiandae commodi perferendis et itaque?",
+          answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!"
+        },
+        {
+          question: "Similique fugiat cumque?",
+          answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!"
+        },
+        {
+          question: "Impedit iusto vitae dolorum, nostrum fugit?",
+          answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!"
         },
         {
           question: "Bombana é gay?",
           answer: "Sim!"
         },
       ],
-      itemsPerPage: 3,
+      itemsPerPage: 6,
       currentPage: 1
     };
   },
