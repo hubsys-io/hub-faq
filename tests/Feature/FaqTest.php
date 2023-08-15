fca0cbc2e358dbad31733b91e7d6429523a55c4c
Lucas Morales <lucasvmorales@protonmail.com>
2023-08-15 19:24:47 -0300

    test: testing faq connection

---
+++ tests/Feature/FaqTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\Faq;
+
+class FaqTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testFaqConnection(): void
+    {
+        $faqs = Faq::all();
+
+        foreach ($faqs as $faq) {
+            $this->assertIsInt($faq->id);
+        }
+    }
+}
